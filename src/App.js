d241b5ae4c4514c1ea579c6ab1e4a5650ffdf3a6
Roy Chng <roychng21@gmail.com>
2025-05-09 01:02:40 +0800

    added songs & bg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import songs from "./songs";
+import headerImg from "./imgs/name-the-song.png";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
+function getRandomSong() {
+  return songs[Math.floor(Math.random() * songs.length)];
+}
+
+let selectedSongs = [];
 
 function App() {
-  const [song, setSong] = useState(songs[0]);
+  const [song, setSong] = useState(getRandomSong());
   const [reveal, setReveal] = useState(false);
 
-  function nextSong(){
-    setSong(songs[Math.floor(Math.random()*songs.length)]);
-    setReveal(false);
+  function nextSong() {
+    const prevSongName = song.name;
+
+    selectedSongs.push(prevSongName);
+    selectedSongs = selectedSongs.slice(-3); // Only want to keep track of 3 most recently selected songs
+    console.log(selectedSongs);
+    while (true) {
+      const newSong = getRandomSong();
+      if (!selectedSongs.includes(newSong.name)) {
+        setSong(newSong);
+        setReveal(false);
+        break;
+      }
+    }
   }
 
   return (
     <main>
-      <h1>Name The Song</h1>
-      { !reveal ? <audio controls src={song.file}>   </audio> : <p>{song.name}</p>}
-    
-      <button class="btn btn-secondary btn-wide" onClick={() => setReveal((r) => !r)}>Reveal Song</button>
-      <button class="btn btn-wide btn-soft" onClick={nextSong}>Next Song</button>
+      <img src={headerImg} className="header-img" />
 
+      {!reveal ? (
+        <AudioPlayer
+          autoPlay={false}
+          showJumpControls={false}
+          showSkipControls={false}
+          src={song.file}
+        />
+      ) : (
+        <div role="alert" class="alert alert-vertical sm:alert-horizontal">
+          <img className="song-img" src={song.cover} />
+          <div>
+            <h3 class="font-bold">{song.name}</h3>
+            <div class="text-xs">by {song.artist}</div>
+          </div>
+        </div>
+      )}
+
+      <button
+        class="btn btn-secondary btn-wide tooltip"
+        data-tip="Make a guess of the song before revealing it!"
+        onClick={() => setReveal((r) => !r)}
+      >
+        Reveal Song
+      </button>
+      <button class="btn btn-wide btn-soft" onClick={nextSong}>
+        Next Song
+      </button>
     </main>
   );
 }
--- src/songs.js
+++ src/songs.js
@@ -1,12 +1,135 @@
-class Song{
-    constructor(name, file){
-        this.name = name;
-        this.file = file;
-    }
+class Song {
+  constructor(name, artist, file, cover) {
+    this.name = name;
+    this.artist = artist;
+    this.file = "songs/" + file;
+    this.cover = "covers/" + cover;
+  }
 }
 
-export default  [
-    new Song('Die with a smile', 'songs/Mellow Jazz.mp3'),
-    new Song('Woeee', 'songs/The Melancholic Lofi.wav'),
-]
-
+export default [
+  new Song(
+    "Die With A Smile",
+    "Lady Gaga & Bruno Mars",
+    "Lady Gaga, Bruno Mars - Die With A Smile (Official Music Video).m4a",
+    "die-with-a-smile.png"
+  ),
+  new Song(
+    "Birds Of A Feather",
+    "Billie Eilish",
+    "Billie Eilish - BIRDS OF A FEATHER (Official Music Video).m4a",
+    "birds-of-a-feather.jpg"
+  ),
+  new Song(
+    "Beautiful Things",
+    "Benson Boone",
+    "Benson Boone - Beautiful Things (Official Music Video).m4a",
+    "beautiful-things.png"
+  ),
+  new Song(
+    "Lose Control",
+    "Teddy Swims",
+    "Teddy Swims - Lose Control (The Village Sessions).m4a",
+    "lose-control.png"
+  ),
+  new Song(
+    "That’s So True",
+    "Gracie Abrams",
+    "That’s So True.m4a",
+    "thats-so-true.jpg"
+  ),
+  new Song(
+    "Sailor Song",
+    "Gigi Perez",
+    "Gigi Perez - Sailor Song [Official Audio].m4a",
+    "sailor-song.png"
+  ),
+  new Song(
+    "No One Noticed",
+    "The Marías",
+    "The Marías - No One Noticed (Visualizer).m4a",
+    "no-one-noticed.jpg"
+  ),
+  new Song(
+    "Too Sweet",
+    "Hozier",
+    "Hozier - Too Sweet (Official Video).m4a",
+    "too-sweet.jpg"
+  ),
+  new Song(
+    "Viva La Vida",
+    "Coldplay",
+    "Coldplay - Viva La Vida (Official Video).m4a",
+    "viva-la-vida.jpg"
+  ),
+  new Song("Creep", "Radiohead", "Radiohead - Creep.m4a", "creep.png"),
+  new Song(
+    "Sweater Weather",
+    "The Neighbourhood",
+    "The Neighbourhood - Sweater Weather (Lyrics).m4a",
+    "sweather-weather.jpg"
+  ),
+  new Song(
+    "Blue",
+    "Yung Kai",
+    "yung kai - blue (official music video).m4a",
+    "blue.png"
+  ),
+  new Song(
+    "Stargazing",
+    "Myles Smith",
+    "Myles Smith - Stargazing (Lyric Video).m4a",
+    "stargazing.jpg"
+  ),
+  new Song(
+    "Dreams",
+    "Fleetwood Mac",
+    "Dreams (2004 Remaster).m4a",
+    "dreams.jpg"
+  ),
+  new Song(
+    "Slow Dancing In a Burning Room",
+    "John Mayer",
+    "Slow Dancing In a Burning Room- John Mayer (With Lyrics).m4a",
+    "slow-dancing-in-a-burning-room.jpg"
+  ),
+  new Song(
+    "I Wanna Be Yours",
+    "Arctic Monkeys",
+    "I Wanna Be Yours.m4a",
+    "i-wanna-be-yours.jpg"
+  ),
+  new Song(
+    "Everybody Wants To Rule The World",
+    "Tears For Fears",
+    "Tears For Fears - Everybody Wants To Rule The World (Live) 2022.m4a",
+    "everybody-wants-to-rule-the-world.jpg"
+  ),
+  new Song(
+    "A Sky Full Of Stars",
+    "Coldplay",
+    "Coldplay - A Sky Full Of Stars (Official audio).m4a",
+    "sky-full-of-stars.png"
+  ),
+  new Song(
+    "Somewhere Only We Know",
+    "Keane",
+    "Keane - Somewhere Only We Know (Lyrics).m4a",
+    "somewhere-only-we-know.jpg"
+  ),
+  new Song(
+    "Back To December",
+    "Taylor Swift",
+    "Taylor Swift - Back To December (Taylor's Version) (Lyric Video).m4a",
+    "back-to-december.jpg"
+  ),
+  new Song("Bags", "Clairo", "Clairo - Bags.m4a", "bags.jpg"),
+  new Song(
+    "I Love You So",
+    "The Walters",
+    "Playing God.m4a",
+    "love-you-so.jpg"
+  ),
+  // new Song("Playing God", "Polyphia", "I Love You So.m4a", "playing-god.jpg"),
+  // new Song("Feel It", "d4vd", "I Love You So.m4a", "playing-god.jpg"),
+];
